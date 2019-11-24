70b60f9f7e4bcd4d3d15ca4ed994af927abf42e1
Katherine Pully <kpully@gmail.com>
2019-11-24 16:18:26 -0600

    fixing criteria errors

--- minidb/criteria.py
+++ minidb/criteria.py
@@ -1,6 +1,5 @@
 import re
 
-<<<<<<< HEAD
 class Criteria():
     def __init__(self, criteria_str):
         comparators = "[=<>!=≥≤]"
@@ -15,18 +14,3 @@ class Criteria():
     def __str__(self):
         return "table 1: %s, field: %s\ntable 2: %s, field: %s" % (self.t1, self.t1_field,self.t2, self.t2_field)
         
-  
-=======
-
-class Criteria:
-
-    def __init__(self, criteria_str):
-        comparators = "[=<>!=≥≤]"
-        self.table1_name = None
-        self.table1_field = None
-        self.table2 = None
-        self.comparator = None
-        self.table1_name = criteria_str.split(".")[0]
-        table1_field = criteria_str.split(",")[1]
-        self.table1_field = filter(None, re.split(comparators, table1_field))
->>>>>>> master
