639ca0eb7e9afbb7f588c12393a598c93bbb957f
Katherine Pully <kpully@gmail.com>
2019-11-29 15:29:15 -0600

    Update database.py

--- minidb/database.py
+++ minidb/database.py
@@ -311,7 +311,7 @@ class Database:
         :return: None
         """
         if not self.__exists(in_table_name):
-            print("Table %s not found" % table)
+            print("Table %s not found" % in_table_name)
             return False
         in_table = self.__get_table(in_table_name)
         out_table=in_table.avg(out_table_name,column)
